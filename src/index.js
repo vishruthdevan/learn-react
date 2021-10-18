7ae5ad82b872ea05b26aed9467f98a6bc3d2ba4c
Vishruth Devan <vishruthdevan@gmail.com>
2021-10-18 22:47:32 +0530

    Mini project - Booklist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
-  return <h3>THE SIGMA MALE IS A LIGER</h3>;
+function BookList() {
+  return(
+    <section>
+      <Book />
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+}
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/516pmXNNmCL._SX324_BO1,204,203,200_.jpg" alt="" />
+const Title = () => <h1>The Subtle Art of Not Giving a Fuck</h1>
+const Author = () => <h4>Mark Manson</h4>
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
