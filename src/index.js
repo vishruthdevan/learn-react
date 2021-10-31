074be56d8933343b53f67c9bd498f5cba7c87194
Vishruth Devan <vishruthdevan@gmail.com>
2021-10-31 22:35:55 +0530

    Reduce nested components and add styling

--- src/index.js
+++ src/index.js
@@ -14,17 +14,15 @@ function BookList() {
 }
 
 const Book = () => {
+  
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src="https://images-na.ssl-images-amazon.com/images/I/516pmXNNmCL._SX324_BO1,204,203,200_.jpg" alt="" />
+      <h1>The Subtle Art of Not Giving a Fuck</h1>
+      <h4>Mark Manson</h4>
     </article>
   );
 }
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/516pmXNNmCL._SX324_BO1,204,203,200_.jpg" alt="" />
-const Title = () => <h1>The Subtle Art of Not Giving a Fuck</h1>
-const Author = () => <h4>Mark Manson</h4>
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
