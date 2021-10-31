52fe4c4d3a66a3f2efb39ec55c91ad3063632e84
Vishruth Devan <vishruthdevan@gmail.com>
2021-10-31 23:09:44 +0530

    Add props and styling to img

--- src/index.js
+++ src/index.js
@@ -2,24 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import './index.css';
+
+const firstBook = {
+  title: 'The Subtle Art of Not Giving a F*ck',
+  author: 'Mark Manson',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/516pmXNNmCL._SX324_BO1,204,203,200_.jpg'
+}
+
+const secondBook = {
+  title: "The Art of the Deal",
+  author: "Robert Kiyosaki",
+  img: "https://images-na.ssl-images-amazon.com/images/I/61o7T0YhPJS.jpg"
+}
+
+
 function BookList() {
   return(
     <section className='booklist'>
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book 
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
       <Book />
       <Book />
     </section>
   );
 }
 
-const Book = () => {
-  
+const Book = (props) => {
   return (
     <article className='book'>
-      <img src="https://images-na.ssl-images-amazon.com/images/I/516pmXNNmCL._SX324_BO1,204,203,200_.jpg" alt="" />
-      <h1>The Subtle Art of Not Giving a Fuck</h1>
-      <h4>Mark Manson</h4>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 }
