2f7cb7d3e55d21c2526bf3e5998483b17cd73ded
Vishruth <vishruthdevan@gmail.com>
2021-11-13 21:26:08 +0530

    Rendering props from list

--- src/index.js
+++ src/index.js
@@ -1,55 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import './index.css';
+import "./index.css";
 
-const firstBook = {
-  title: 'The Subtle Art of Not Giving a F*ck',
-  author: 'Mark Manson',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/516pmXNNmCL._SX324_BO1,204,203,200_.jpg'
-}
+const books = [
+  {
+    id: 1,
+    title: "The Subtle Art of Not Giving a F*ck",
+    author: "Mark Manson",
+    img: "https://images-na.ssl-images-amazon.com/images/I/516pmXNNmCL._SX324_BO1,204,203,200_.jpg",
+  },
 
-const secondBook = {
-  title: "The Art of the Deal",
-  author: "Robert Kiyosaki",
-  img: "https://images-na.ssl-images-amazon.com/images/I/61o7T0YhPJS.jpg"
-}
+  {
+    id: 2,
+    title: "The Art of the Deal",
+    author: "Robert Kiyosaki",
+    img: "https://images-na.ssl-images-amazon.com/images/I/61o7T0YhPJS.jpg",
+  },
 
+  {
+    id: 3,
+    title: "The Da Vince Code",
+    author: "Dan Brown",
+    img: "https://images-na.ssl-images-amazon.com/images/I/61voOxiNf9L.jpg",
+  },
+];
 
 function BookList() {
-  return(
-    <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt perferendis ab veniam in dignissimos tenetur iste voluptas vel iure modi!
-      </p>
-      </Book>
-      <Book 
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book />
-      <Book />
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        // const { img, title, author } = book;
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const {img, title, author, children} = props;
+  const { img, title, author, children } = props;
+
   return (
-    <article className='book'>
+    <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
       {children}
     </article>
   );
-}
-
+};
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
