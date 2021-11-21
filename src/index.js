1285d17c32c4995cdc3f622d347e075e3571715c
Vishruth <vishruthdevan@gmail.com>
2021-11-21 21:28:43 +0530

    Split components across book.js, books.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { books } from "./books";
+import Book from "./book";
 
 import "./index.css";
-
-const books = [
-  {
-    id: 1,
-    title: "The Subtle Art of Not Giving a F*ck",
-    author: "Mark Manson",
-    img: "https://images-na.ssl-images-amazon.com/images/I/516pmXNNmCL._SX324_BO1,204,203,200_.jpg",
-  },
-
-  {
-    id: 2,
-    title: "The Art of the Deal",
-    author: "Robert Kiyosaki",
-    img: "https://images-na.ssl-images-amazon.com/images/I/61o7T0YhPJS.jpg",
-  },
-
-  {
-    id: 3,
-    title: "The Da Vince Code",
-    author: "Dan Brown",
-    img: "https://images-na.ssl-images-amazon.com/images/I/61voOxiNf9L.jpg",
-  },
-];
-
 function BookList() {
   return (
     <section className="booklist">
@@ -36,28 +14,5 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, title, author, children }) => {
-  const clickHandler = (e) => {
-    // console.log(author);
-    console.log(e.target);
-  };
-  return (
-    <article
-      className="book"
-      onMouseDown={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        click me
-      </button>
-      {children}
-    </article>
-  );
-};
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
