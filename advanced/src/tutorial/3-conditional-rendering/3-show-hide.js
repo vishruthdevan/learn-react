83a8e76cdd35a97c4ed3bd2d8d4d79b796e5a47e
Vishruth <vishruthdevan@gmail.com>
2022-02-25 23:09:54 +0530

    Conditional render of React components

--- advanced/src/tutorial/3-conditional-rendering/3-show-hide.js
+++ advanced/src/tutorial/3-conditional-rendering/3-show-hide.js
@@ -1,7 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+    const [show, setShow] = useState(false);
+
+    return (
+        <>
+            <button className="btn" onClick={() => setShow(!show)}>
+                show/hide
+            </button>
+            {show && <Item />}
+        </>
+    );
+};
+
+const Item = () => {
+    return (
+        <div>
+            <h1>window</h1>
+            <h3>size: </h3>
+        </div>
+    );
 };
 
 export default ShowHide;
