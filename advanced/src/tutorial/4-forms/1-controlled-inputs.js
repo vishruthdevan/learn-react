a8a94bcf7b04b2f274af751284f7b989f75592b2
Vishruth <vishruthdevan@gmail.com>
2022-03-01 23:22:33 +0530

    Handle multiple controlled inputs

--- advanced/src/tutorial/4-forms/1-controlled-inputs.js
+++ advanced/src/tutorial/4-forms/1-controlled-inputs.js
@@ -8,10 +8,23 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
     const [firstName, setFirstName] = useState("");
     const [email, setEmail] = useState("");
+    const [people, setPeople] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(firstName, email);
+        if (firstName && email) {
+            const person = {
+                id: new Date().getTime().toString(),
+                firstName,
+                email,
+            };
+            setPeople((people) => {
+                return [...person, person];
+            });
+            setFirstName("");
+            setEmail("");
+            console.log(person);
+        } else console.log("null values");
     };
     return (
         <>
@@ -43,6 +56,15 @@ const ControlledInputs = () => {
                     </div>
                     <button type="submit">add person</button>
                 </form>
+                {people.map((person) => {
+                    const { id, firstName, email } = person;
+                    return (
+                        <div className="item" key={id}>
+                            <h4>{firstName}</h4>
+                            <p>{email}</p>
+                        </div>
+                    );
+                })}
             </article>
         </>
     );
