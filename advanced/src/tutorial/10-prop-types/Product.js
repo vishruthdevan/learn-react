c90854c4bf629dfecf7f341c389a146e7173e76d
Vishruth Devan <vishruthdevan@gmail.com>
2022-05-26 23:54:36 +0530

    Proptypes setup

--- advanced/src/tutorial/10-prop-types/Product.js
+++ advanced/src/tutorial/10-prop-types/Product.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({image, name, price}) => {
+  return <article className='product'>
+    <img src={image.url} alt={name} />
+    <h4>{name}</h4>
+    <p>${price}</p>
+  </article>;
 };
 
 export default Product;
--- advanced/src/tutorial/10-prop-types/index.js
+++ advanced/src/tutorial/10-prop-types/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Product from './Product'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import { useFetch } from '../9-custom-hooks/2-useFetch'
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-prop-types-example'
 
 const Index = () => {
-  const { products } = useFetch(url)
+  const { data:products } = useFetch(url)
   return (
     <div>
       <h2>products</h2>
