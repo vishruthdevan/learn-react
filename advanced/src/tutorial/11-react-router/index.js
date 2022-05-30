d3460dd809bf69bf1e6a75bc6dcce8b6da4c2ccc
Vishruth Devan <vishruthdevan@gmail.com>
2022-05-30 21:51:10 +0530

    Create react router app skeleton

--- advanced/src/tutorial/11-react-router/index.js
+++ advanced/src/tutorial/11-react-router/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React from "react";
 // react router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Switch,
+} from "react-router-dom";
 // pages
-import Home from './Home';
-import About from './About';
-import People from './People';
-import Error from './Error';
-import Person from './Person';
+import Home from "./Home";
+import About from "./About";
+import People from "./People";
+import Error from "./Error";
+import Person from "./Person";
 // navbar
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/people" element={<People />} />
+                <Route path="/error" element={<Error />} />
+                <Route path="/person" element={<Person />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default ReactRouterSetup;
